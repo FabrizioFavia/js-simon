6d5cf6c9926d79464de3bbb5314348cb4abc21ab
FabrizioFavia <personal.favia@gmail.com>
2023-05-02 08:40:34 +0200

    fix(js): changed function names.

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ let playBtn = document.getElementById("btnStart");
 let userNumber = document.getElementById("btnGiveNumbers");
 
 /* FUNZIONE PER GENERARE NUMERI RANDOM E INSERIRLI NELL'ARRAY */
-function generateDifferentNumbers(arrayElements) {
+function generateRandomNum(arrayElements) {
     let arrayNumber = [];
     let number = null;
 
@@ -12,30 +12,27 @@ function generateDifferentNumbers(arrayElements) {
 
         number = Math.floor(Math.random() * 100) + 1;
 
-        if (arrayNumber.includes(number) == false) {
+        if (!arrayNumber.includes(number)) {
             arrayNumber.push(number);
         }
     }
     return arrayNumber;
 }
-/* FUNZIONE PER CREARE BOX E INSERIRE NUMERI */
-function generateBox(arrayName) {
 
-    for (let i = 0; i < arrayName.length; i++) {
-        const number = arrayName[i];
+/* FUNZIONE PER CREARE E INSERIRE ELEMENTI */
+function generateElement(sourceArrayName, htmlElement, className) {
 
-        let box = document.createElement("div");
-        box.classList.add("boxNumber");
+    for (let i = 0; i < sourceArrayName.length; i++) {
+        const number = sourceArrayName[i];
+
+        let box = document.createElement(htmlElement);
+        box.classList.add(className);
         box.append(number);
         mainBox.append(box);
     }
 
 }
-/* FUNZIONE CHE ELIMINA I NUMERI */
-function clear() {
-    mainBox.innerHTML = "";
 
-}
 /* FUNZIONE PER DISABILITARE/ABILITARE BOTTONE */
 function disablePlay(off) {
     if (off) {
@@ -48,14 +45,14 @@ function disablePlay(off) {
 /* FUNZIONE PER CHIEDERE I NUMERI ALL'UTENTE E PER INSERIRLI NELL'ARRAY */
 function getUserNumber(arrayElements) {
     let userNumberArray = [];
-    let i = 0; 
-    while(userNumberArray.length < arrayElements.length) {
+    let i = 0;
+    while (userNumberArray.length < arrayElements.length) {
 
         let userNumber = parseInt(prompt(`Inserisci il numero ${i + 1} di ${arrayElements.length}`));
 
-        if (userNumber!= null  && !isNaN(userNumber)) {
+        if (userNumber != null && !isNaN(userNumber)) {
             userNumberArray.push(userNumber);
-            i +=1;
+            i += 1;
         }
     }
     return userNumberArray;
@@ -64,13 +61,13 @@ function getUserNumber(arrayElements) {
 
 /* FUNZIONE PER CONTROLLARE SE I NUMERI DELL'UTENTE 
 SONO UGUALI A QUELLI PRESENTI NELL'ARRAY DI NUMERI CASUALI */
-function checkArray(array1, array2){
+function checkArray(array1, array2) {
 
     let finalArray = [];
 
     for (let i = 0; i < array1.length; i++) {
         const randomNumber = array1[i];
-    
+
         if (array2.includes(randomNumber)) {
             finalArray.push(randomNumber);
         }
@@ -80,15 +77,24 @@ function checkArray(array1, array2){
 }
 
 /* FUNZIONE PER INSERIRE ELEMENTI NEL DOM */
-function insertResult(finalArray){
+function insertResult(finalArray) {
 
-    
-    mainBox.innerHTML = 
-    `<div class= "resultContainer">
-        <p> Hai indovinato ${finalArray.length} numeri</p> 
-        <p> I numeri sono: ${finalArray.toString()}.</p>
-    </div>`
+    if (finalArray.length <= 1) {
+        mainBox.innerHTML =
+            `<div class= "resultContainer">
+            <p> Hai indovinato ${finalArray.length} numero</p> 
+            <p> Il numero è: ${finalArray.toString()}.</p>
+            </div>`;
+    } else {
+        mainBox.innerHTML =
+            `<div class= "resultContainer">
+            <p> Hai indovinato ${finalArray.length} numeri</p> 
+            <p> I numeri sono: ${finalArray.toString()}.</p>
+            </div>`;
+    }
+
 }
+
 
 
 /********************************************** FUNZIONE PRINCIPALE *****************************************************************************/
@@ -96,16 +102,19 @@ function insertResult(finalArray){
 playBtn.addEventListener("click", function () {
 
     disablePlay(true);
-    clear();
-    let randomArray = generateDifferentNumbers(5);
-    generateBox(randomArray);
-    setTimeout(() => {
-        clear();
+    mainBox.innerHTML = "";
+    let randomNumberArray = generateRandomNum(5);
+    generateElement(randomNumberArray, "div", "boxNumber");
+
+    setTimeout(() => { /* Qui viene inserito il timer */
+        /* Passano 5 secondi ed esegue i comandi inseriti al suo interno */
+        mainBox.innerHTML = "";
         disablePlay(false);
-        setTimeout(() => {
-            let userNumberArray = getUserNumber(randomArray);
-            let finalArray = checkArray(randomArray, userNumberArray);
+        setTimeout(() => { /* Qui viene inserito un secondo timer */
+            /* Questo parte poco dopo lo scadere del primo */
+            let userNumberArray = getUserNumber(randomNumberArray);
+            let finalArray = checkArray(randomNumberArray, userNumberArray);
             insertResult(finalArray)
         }, 300);
-    }, 5 * 1000);
+    }, 10 * 1000);
 });
